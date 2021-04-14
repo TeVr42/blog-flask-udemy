94b6aff7a136c5bb783687b87e39528ca03e3c98
Terka <tervrb@seznam.cz>
2021-04-14 14:41:43 +0200

    Try to make it work (login/register)

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 from flask_gravatar import Gravatar
 from functools import wraps
 import os
+import requests
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
